6c27a8217a21f493095923d07e2fad8b9f0211dc
Partha Pratim Sarma <64675650+partha8@users.noreply.github.com>
2023-01-05 22:36:02 +0530

    feat: added loader

---
+++ components/Loader.js
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "../public/ps.png";
+import Image from "next/image";
+
+export const Loader = () => {
+  return (
+    <div className="h-[100vh] bg-[#111827] flex items-center justify-center">
+      <section className="relative flex items-center justify-center">
+        <Image alt="logo" width={250} src={logo} />
+
+        <div class="circle">
+          <div class="circle__item">
+            <svg class="circle__spinner" viewBox="0 0 50 50">
+              <circle
+                class="path"
+                cx="25"
+                cy="25"
+                r="23"
+                fill="none"
+                stroke="#93bfec"
+                stroke-width="11"
+              ></circle>
+            </svg>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,20 @@ import logo from "../public/ps.png";
 import { Introduction, Projects, Skills, AboutMe } from "../components";
 import { motion } from "framer-motion";
 import { list, item } from "../animation.config";
+import { useEffect, useState } from "react";
+import { Loader } from "../components/Loader";
 
 export default function Home() {
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoader(false);
+    }, 6000);
+
+    return () => clearTimeout(timer);
+  }, [loader]);
+
   return (
     <div className="dark scroll-smooth h-screen w-screen overflow-y overflow-x-hidden">
       <Head>
@@ -33,44 +45,50 @@ export default function Home() {
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
-      <motion.main
-        initial="hidden"
-        animate="visible"
-        variants={list}
-        className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40 "
-      >
-        <motion.nav
-          variants={item}
-          className="pt-10 flex justify-between items-center dark:text-white"
-        >
-          <motion.div>
-            <Image alt="logo" width={80} height={80} src={logo} />
-          </motion.div>
-          <motion.a
-            className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8 h-fit w-fit"
-            href="https://drive.google.com/file/d/1TJa9pp85Yh5xylkWdCfyrYsG2ztjCSMP/view?usp=sharing"
-            target="__blank"
+      {loader ? (
+        <Loader />
+      ) : (
+        <>
+          <motion.main
+            initial="hidden"
+            animate="visible"
+            variants={list}
+            className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40 "
           >
-            Resume
-          </motion.a>
-        </motion.nav>
+            <motion.nav
+              variants={item}
+              className="pt-10 flex justify-between items-center dark:text-white"
+            >
+              <motion.div>
+                <Image alt="logo" width={80} height={80} src={logo} />
+              </motion.div>
+              <motion.a
+                className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8 h-fit w-fit"
+                href="https://drive.google.com/file/d/1TJa9pp85Yh5xylkWdCfyrYsG2ztjCSMP/view?usp=sharing"
+                target="__blank"
+              >
+                Resume
+              </motion.a>
+            </motion.nav>
 
-        <motion.div variants={item}>
-          <Introduction />
+            <motion.div variants={item}>
+              <Introduction />
 
-          <AboutMe />
+              <AboutMe />
 
-          <Skills />
+              <Skills />
 
-          <Projects />
-        </motion.div>
-      </motion.main>
-      <motion.footer className=" px-10 dark:bg-gray-900 md:px-20 lg:px-40 py-6 snap-start">
-        <p className="text-xl text-center leading-8  text-gray-800 dark:text-gray-200">
-          Made with love and <span className=" text-teal-500 ">NextJS</span> by{" "}
-          <span className=" text-teal-500 ">Partha</span>
-        </p>
-      </motion.footer>
+              <Projects />
+            </motion.div>
+          </motion.main>
+          <motion.footer className=" px-10 dark:bg-gray-900 md:px-20 lg:px-40 py-6 snap-start">
+            <p className="text-xl text-center leading-8  text-gray-800 dark:text-gray-200">
+              Made with love and <span className=" text-teal-500 ">NextJS</span>{" "}
+              by <span className=" text-teal-500 ">Partha</span>
+            </p>
+          </motion.footer>
+        </>
+      )}
     </div>
   );
 }
